f1fd62e014a98bee1ac1babebb30b0f7d786f27e
haris priambada <primbodoharis@gmail.com>
2024-02-27 14:58:39 +0700

    selesai bagian penting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
+const ItemTodo = [
+  {
+    id: 1,
+    todo: "memakan roti",
+    condition: false,
+  },
+  {
+    id: 2,
+    todo: "memakan roti",
+    condition: false,
+  },
+  {
+    id: 3,
+    todo: "memakan roti",
+    condition: false,
+  },
+];
 function App() {
+  const [todoItems, setTodoItem] = useState(ItemTodo);
+  const [inputTodo, setInputTodo] = useState("");
+
+  function onDelete(todo) {
+    setTodoItem((todoItems) =>
+      todoItems.filter((todoItem) => todoItem.id !== todo)
+    );
+    console.log(todo);
+  }
+  function addTodo(todo) {
+    setTodoItem((todoItems) => [...todoItems, todo]);
+  }
+  function handleDone(id) {
+    setTodoItem((todoItems) =>
+      todoItems.map((todoitem) =>
+        todoitem.id === id
+          ? { ...todoitem, condition: !todoitem.condition }
+          : todoitem
+      )
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FormInput
+        inputTodo={inputTodo}
+        setInputTodo={setInputTodo}
+        addTodo={addTodo}
+      />
+
+      <ItemList todoItems={todoItems} onDelete={onDelete} onDone={handleDone} />
     </div>
   );
 }
 
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormInput({ inputTodo, setInputTodo, addTodo }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const id = new Date();
+    const neTodo = {
+      id,
+      todo: inputTodo,
+      status: false,
+    };
+    setInputTodo("");
+    addTodo(neTodo);
+  }
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="descr">
+        <label>description</label>
+        <br></br>
+        <input
+          type="text"
+          value={inputTodo}
+          onChange={(e) => setInputTodo(e.target.value)}
+        />
+        <Button className="button">Add</Button>
+      </div>
+    </form>
+  );
+}
+
+function ItemList({ todoItems, onDelete, onDone }) {
+  return todoItems.map((todo) => (
+    <Item todoItem={todo} onDelete={onDelete} onDone={onDone} />
+  ));
+}
+function Item({ todoItem, onDelete, onDone }) {
+  return (
+    <div className="input">
+      <input
+        type="checkbox"
+        value={todoItem.condition}
+        onChange={() => onDone(todoItem.id)}
+      />
+      <input
+        value={todoItem.todo}
+        className={todoItem.condition ? `todo line` : `todo`}
+        type="text"
+      />
+      <button className="button" onClick={() => onDelete(todoItem.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
 export default App;
